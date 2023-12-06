636a4752f27f729e05dfe3774a60f6e5b8990213
Micah N Gorrell <m@minego.net>
2023-12-06 12:07:59 -0700

    Removing treesitter, and relying on system packages for it instead

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -33,7 +33,6 @@ return {
 			'nvim-lua/plenary.nvim',
 			'nvim-lua/popup.nvim',
 			'nvim-telescope/telescope.nvim',
-			'nvim-treesitter/nvim-treesitter',
 		},
 		config = function()
 			require'telescope'.load_extension'goimpl'

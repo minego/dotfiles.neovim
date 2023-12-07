9062f3e92171918c95865169159059e6917b67fb
Micah N Gorrell <m@minego.net>
2023-12-06 17:03:11 -0700

    Revert "Removing treesitter, and relying on system packages for it instead"
    This reverts commit 636a4752f27f729e05dfe3774a60f6e5b8990213.

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -33,6 +33,7 @@ return {
 			'nvim-lua/plenary.nvim',
 			'nvim-lua/popup.nvim',
 			'nvim-telescope/telescope.nvim',
+			'nvim-treesitter/nvim-treesitter',
 		},
 		config = function()
 			require'telescope'.load_extension'goimpl'
